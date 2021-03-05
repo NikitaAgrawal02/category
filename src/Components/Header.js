c4c4ca2fd0f9dae72a0e25b1ae9ce11a6ce11ffa
Nikita <nikita.agrawal@sarvika.com>
2021-03-05 09:46:01 +0530

    second

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,12 +6,12 @@ const Header = () => {
             <a className="active item">
                 Home
             </a>
-            <a classNameName="item">
+            {/* <a className="item">
                 Messages
             </a>
             <a className="item">
                 Friends
-            </a>
+            </a> */}
             <div className="right menu">
                 <div className="item">
                     <div className="ui icon input">
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NavDropdown, Form, FormControl, Button, Nav, Navbar } from 'react-bootstrap';
+const Sidebar = () => {
+    return (
+
+        <div>TEST</div>
+    )
+}
+
+export default Sidebar
